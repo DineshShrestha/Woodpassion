36c9d0444995c1ee8d090d5debd0b268776bd78e
sdin <sdin@LAPTOP-DINESH>
2024-08-30 14:23:23 +0200

    add different media screen/toggle menu with javascript animation on section

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,15 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY> 150);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlinks = document.querySelector('.navlinks');
+menu.onclick= ()=>{
+    menu.classList.toggle('bx-x');
+    navlinks.classList.toggle('open')
+}
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlinks.classList.remove('open');
+}
